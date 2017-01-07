07c5a49082f30914f356deca7c2fb6226bed9dd1
NutsLyam <natalyailyamakova@yandex.ru>
2017-01-07 17:20:45 +0300

    Add files via upload

--- DataMiningBus.py
+++ DataMiningBus.py
@@ -1,12 +1,10 @@
-#разбивает полученное множество интервалов с наименьшими скоростями
-#на кластеры
 import numpy as np
 import io
 from sklearn.cluster import KMeans
 from datetime import datetime
 
 with open('D:\\DataMiningProject\\transport\\pointsy.txt',  'r',encoding='utf-8' ) as f:
-   #f1=io.TextIOWrapper(f,'utf-8')
+   
    X = np.array([(float(line.strip().split('\t')[0]),
                     float(line.strip().split('\t')[1])) for line in f])
                                                     
@@ -16,6 +14,13 @@ kmeans.fit(X)
 
 centroids = kmeans.cluster_centers_
 labels = kmeans.labels_
-l = sorted(centroids, key = lambda x:x[0])
-print(l)
+l = sorted(centroids, key = lambda x:x[0],reverse=True)
+np.savetxt('C:\\Users\\Наташа\\Documents\\Matlab\\centroids.txt',l)
+f1=open('D:\\DataMiningProject\\transport\\cent.txt','w')
+#for li in centroids:
+ #   print(li)
+    
 
+
+for li in l:
+    print(li)
